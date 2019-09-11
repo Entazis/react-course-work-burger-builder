0ad8a319207433c3dd81f7be3ca4aeafbbc786b9
Szabó Bence <szabo.bence.tat@gmail.com>
2019-09-11 14:35:46 +0200

    added text if there aren't any ingredients

--- src/components/BurgerBuilder/Burger/Burger.js
+++ src/components/BurgerBuilder/Burger/Burger.js
@@ -10,11 +10,12 @@ import  { ingredients }
 const burger = (props) => {
     const transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => [...Array(props.ingredients[igKey])].map((_, i) => <BurgerIngredient key={igKey + i} type={igKey} />));
+    const isEmpty = a => Array.isArray(a) && a.every(isEmpty);
 
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type={ingredients.breadTop}/>
-            {transformedIngredients}
+            {(!isEmpty(transformedIngredients)) ? transformedIngredients : "Please start adding ingredients!"}
             <BurgerIngredient type={ingredients.breadBottom}/>
         </div>
     );
