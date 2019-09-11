48791bab24fbf4767e98de4dfde75b45bab75c55
Szabó Bence <szabo.bence.tat@gmail.com>
2019-09-11 10:33:41 +0200

    added ingredients property transformation

--- src/components/BurgerBuilder/Burger/Burger.js
+++ src/components/BurgerBuilder/Burger/Burger.js
@@ -1,15 +1,18 @@
 import React
   from 'react';
+import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import ingredientConstants
   from './BurgerIngredient/constants';
 
 const burger = (props) => {
-  //TODO: transform props.ingredients to array to loop and return <ingredient> jsx
-  return (
-      <div>
-        <BurgerIngredient type={ingredientConstants.breadTop}/>
+  const transformedIngredients = Object.keys(props.ingredients)
+      .map(igKey => [...Array(props.ingredients[igKey])].map((_, i) => <BurgerIngredient key={igKey + i} type={igKey} />));
 
+  return (
+      <div className={classes.Burger}>
+        <BurgerIngredient type={ingredientConstants.breadTop}/>
+        {transformedIngredients}
         <BurgerIngredient type={ingredientConstants.breadBottom}/>
       </div>
   );
