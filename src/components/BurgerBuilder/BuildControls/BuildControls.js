4392450ba8f84fed77fe3383adffb7af611c0222
Szabó Bence <szabo.bence.tat@gmail.com>
2019-09-11 11:23:12 +0200

    added buildControl

--- src/components/BurgerBuilder/BuildControls/BuildControls.js
+++ src/components/BurgerBuilder/BuildControls/BuildControls.js
@@ -1,10 +1,14 @@
 import React
   from 'react';
+import classes from './BuildControls.module.css';
+import { controls }
+  from '../constants';
+import BuildControl from './BuildControl/BuildControl';
 
 const buildControls = () => {
   return (
-      <div>
-        Build Controls
+      <div className={classes.BuildControls}>
+        {controls.map(control => <BuildControl key={control.label} label={control.label} type={control.type}/>)}
       </div>
   );
 };
