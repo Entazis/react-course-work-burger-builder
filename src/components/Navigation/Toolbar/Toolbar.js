a827214237966df94a1e4b6baa782da54be2c346
Szabó Bence <szabo.bence.tat@gmail.com>
2019-09-19 11:24:20 +0200

    added link to logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,13 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import DrawToggler from '../../UI/SideDrawer/DrawerToggle/DrawerToggle';
+import { Link } from 'react-router-dom';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <DrawToggler clicked={props.clicked}/>
         <div className={classes.Logo}>
-            <Logo />
+            <Link to="/"><Logo /></Link>
         </div>
         <nav className={classes.DesktopOnly}>
             <NavigationItems/>
