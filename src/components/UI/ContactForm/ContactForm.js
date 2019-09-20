0462acbe75c9a2d13f78f38af1cca8dc1e79526a
Szabó Bence <szabo.bence.tat@gmail.com>
2019-09-20 16:05:02 +0200

    added select input type, some styles, form configuration

--- src/components/UI/ContactForm/ContactForm.js
+++ src/components/UI/ContactForm/ContactForm.js
@@ -1,19 +1,84 @@
-import React
+import React, { Component }
     from 'react';
 import Button from '../Button/Button';
 import Input
     from './Input/Input';
 
-const contactForm = (props) => {
-    return (
-        <React.Fragment>
-            <h3>Please enter your contact data</h3>
-            <Input type="text" name="name" displayName="Your name" placeholder="Enter your name" />
-            <Input type="text" name="address" displayName="Your address" placeholder="Enter your address" />
-            <Input type="text" name="phone" displayName="Phone number" placeholder="Enter your phone number" />
-            <Button type={'Success'} clicked={props.clicked}>ORDER</Button>
-        </React.Fragment>
-    );
-};
+class ContactForm extends Component  {
+    state = {
+        contactForm: {
+            name: {
+                inputType: 'text',
+                config: {
+                    type: 'text',
+                    placeholder: 'Enter your name',
+                    value: ''
+                },
+                displayName: 'Your name',
+                isValid: ''
+            },
+            address: {
+                inputType: 'text',
+                config: {
+                    type: 'text',
+                    placeholder: 'Enter your address',
+                    value: ''
+                },
+                displayName: 'Your address',
+                isValid: ''
+            },
+            phone: {
+                inputType: 'text',
+                config: {
+                    type: 'text',
+                    placeholder: 'Enter your name',
+                    value: ''
+                },
+                displayName: 'Phone number',
+                isValid: ''
+            },
+            email: {
+                inputType: 'text',
+                config: {
+                    type: 'email',
+                    placeholder: 'Enter your email address',
+                    value: ''
+                },
+                displayName: 'Email address',
+                isValid: ''
+            },
+            deliveryMethod: {
+                inputType: 'select',
+                config: [
+                    {value: 'fastest', displayValue: 'Fastest'},
+                    {value: 'cheapest', displayValue: 'Cheapest'}
+                ],
+                displayName: 'Delivery Method',
+                isValid: ''
+            }
+        }
+    };
 
-export default contactForm;
+    render() {
+        const form = this.state.contactForm;
+        const inputFields = Object.keys(form).map(formKey => {
+            return (
+                <Input
+                    inputType={form[formKey].inputType}
+                    config={form[formKey].config}
+                    key={formKey}
+                    displayName={form[formKey].displayName}/>
+            )
+        });
+
+        return (
+            <React.Fragment>
+                <h3>Please enter your contact data</h3>
+                {inputFields}
+                <Button type={'Success'} clicked={this.props.clicked}>ORDER</Button>
+            </React.Fragment>
+        )
+    }
+}
+
+export default ContactForm;
--- src/components/UI/ContactForm/Input/Input.js
+++ src/components/UI/ContactForm/Input/Input.js
@@ -2,12 +2,36 @@ import React from 'react';
 import classes from './Input.module.css';
 
 const Input = (props) => {
+    let input = null;
+
+    switch (props.inputType) {
+        case 'input':
+            input = (
+                <input
+                    {...props.config}
+                />
+            );
+            break;
+        case 'select':
+            input = (
+                <select>
+                    {props.config.map(option => <option key={option.value} value={option.value}>{option.displayValue}</option>)}
+                </select>
+            );
+            break;
+        default:
+            input = (
+                <input
+                    {...props.config}
+                />
+            );
+            break;
+
+    }
     return (
         <div className={classes.Input}>
-            <label>{props.displayName}</label>
-            <input
-                {...props}
-            />
+            <label className={classes.Label}>{props.displayName}</label>
+            {input}
         </div>
     );
 };
