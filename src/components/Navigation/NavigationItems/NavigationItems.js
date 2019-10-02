16ccd26bd6a23a2605dd1b508f1bb118c5fda264
Szabó Bence <szabo.bence.tat@gmail.com>
2019-10-02 16:23:02 +0200

    update navbar when logged in

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem link="/">Burger Builder</NavigationItem>
-        <NavigationItem link="/my-orders">Orders</NavigationItem>
-        <NavigationItem link="/auth">Login / Signup</NavigationItem>
-    </ul>
-);
+class navigationItems extends Component {
 
-export default navigationItems;
+    render() {
+        return (
+            <ul className={classes.NavigationItems}>
+                <NavigationItem link="/">Burger Builder</NavigationItem>
+                <NavigationItem link="/my-orders">Orders</NavigationItem>
+                {(this.props.user) ?
+                    <NavigationItem link="/auth">Logout</NavigationItem> :
+                    <NavigationItem link="/auth">Login / Signup</NavigationItem>
+                }
+            </ul>);
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        user: state.auth.user
+    }
+};
+
+export default connect(mapStateToProps)(navigationItems);
